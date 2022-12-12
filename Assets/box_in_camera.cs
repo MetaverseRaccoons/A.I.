6d33f0ec01a58f440d3fb97ecd43ba7a42fa66d6
liseg7 <lise.guilliams@student.kuleuven.be>
2022-12-12 16:22:35 +0100

    setup
    initial project with cube test

---
+++ Assets/box_in_camera.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box_in_camera : MonoBehaviour
+{
+    public Camera temp;
+    MeshRenderer renderer2;
+    Plane[] cameraFrustum; //series of camera planes
+    Collider collider2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer2 = GetComponent<MeshRenderer>();
+        collider2 = GetComponent<Collider>();
+        var bounds = collider2.bounds;
+        cameraFrustum = GeometryUtility.CalculateFrustumPlanes(temp); //returns the planes for the camera --> place it in update if your camera moves
+        if (GeometryUtility.TestPlanesAABB(cameraFrustum, bounds))
+        {
+            renderer2.sharedMaterial.color = Color.green;
+        }
+        else
+        {
+            renderer2.sharedMaterial.color = Color.red;
+
+        }
+    }
+
+
+    void Update()
+    {
+       
+        var bounds = GetComponent<Collider>().bounds;
+        cameraFrustum = GeometryUtility.CalculateFrustumPlanes(temp); //returns the planes for the camera --> place it in update if your camera moves
+        if (GeometryUtility.TestPlanesAABB(cameraFrustum, bounds))
+        {
+            renderer2.sharedMaterial.color = Color.green;
+        }
+        else
+        {
+            renderer2.sharedMaterial.color = Color.red;
+
+        }
+    }
+}
+
+
+
+// Start is called before the first frame update
